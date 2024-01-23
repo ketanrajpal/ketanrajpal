2c63218588056b48952267255787c49d3dc789e6
Ketan Rajpal <ketanrajpal@gmail.com>
2024-01-23 23:50:33 +0000

    Update project.ts

--- sanity/schemas/project.ts
+++ sanity/schemas/project.ts
@@ -1,91 +1,95 @@
-import { orderRankField, orderRankOrdering } from "@sanity/orderable-document-list"
+import {orderRankField, orderRankOrdering} from '@sanity/orderable-document-list'
 
 export const project = {
-    name: "project",
-    type: "document",
-    title: "Project",
-    orderings: [orderRankOrdering],
-    fields: [
+  name: 'project',
+  type: 'document',
+  title: 'Project',
+  orderings: [orderRankOrdering],
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'featured',
+      type: 'boolean',
+      title: 'Featured',
+    },
+    {
+      name: 'shortDescription',
+      type: 'string',
+      title: 'Short Description',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description',
+    },
+    {
+      name: 'date',
+      type: 'date',
+      title: 'Date',
+    },
+    {
+      name: 'URL',
+      type: 'url',
+      title: 'URL',
+    },
+    {
+      name: 'client',
+      type: 'reference',
+      title: 'Client',
+      to: [
         {
-            name: "title",
-            type: "string",
-            title: "Title",
+          type: 'client',
         },
+      ],
+    },
+    {
+      name: 'tags',
+      type: 'array',
+      title: 'Tags',
+      of: [
         {
-            name: "slug",
-            type: "slug",
-            title: "Slug",
-            options: {
-                source: "title",
-                maxLength: 96
-            }
+          type: 'reference',
+          to: [
+            {
+              type: 'tag',
+            },
+          ],
         },
+      ],
+    },
+    {
+      name: 'technologies',
+      type: 'array',
+      title: 'Technologies',
+      of: [
         {
-            name: "featured",
-            type: "boolean",
-            title: "Featured",
+          type: 'reference',
+          to: [
+            {
+              type: 'technology',
+            },
+          ],
         },
-        {
-            name: "description",
-            type: "text",
-            title: "Description",
-        },
-        {
-            name: "date",
-            type: "date",
-            title: "Date",
-        },
-        {
-            name: "URL",
-            type: "url",
-            title: "URL",
-        },
-        {
-            name: "client",
-            type: "reference",
-            title: "Client",
-            to: [
-                {
-                    type: "client",
-                }
-            ]
-        },
-        {
-            name: "tags",
-            type: "array",
-            title: "Tags",
-            of: [
-                {
-                    type: "reference",
-                    to: [
-                        {
-                            type: "tag",
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            name: "technologies",
-            type: "array",
-            title: "Technologies",
-            of: [
-                {
-                    type: "reference",
-                    to: [
-                        {
-                            type: "technology",
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            name: "thumbnail",
-            type: "image",
-            title: "Thumbnail"
-
-        },
-        orderRankField({ type: "project" })
-    ]
-} 
+      ],
+    },
+    {
+      name: 'thumbnail',
+      type: 'image',
+      title: 'Thumbnail',
+    },
+    orderRankField({type: 'project'}),
+  ],
+}
