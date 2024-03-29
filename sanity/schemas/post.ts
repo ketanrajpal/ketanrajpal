bc6f27fca4dd6ce528e569b8692d3927509960ce
Ketan Rajpal <ketanrajpal@gmail.com>
2024-03-29 21:12:34 +0000

    Update post.ts

--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -1,62 +1,67 @@
 export const post = {
-    name: "post",
-    type: "document",
-    title: "Post",
-    fields: [
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 200,
+      },
+    },
+    {
+      name: 'featured',
+      type: 'boolean',
+      title: 'Featured',
+    },
+    {
+      title: 'Banner',
+      name: 'banner',
+      type: 'image',
+    },
+    {
+      title: 'Content',
+      name: 'content',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'category',
+      type: 'array',
+      title: 'Category',
+      of: [
         {
-            name: "title",
-            type: "string",
-            title: "Title",
+          type: 'reference',
+          to: [
+            {
+              type: 'category',
+            },
+          ],
         },
+      ],
+    },
+    {
+      name: 'keywords',
+      type: 'array',
+      title: 'Keywords',
+      of: [
         {
-            name: "slug",
-            type: "slug",
-            title: "Slug",
-            options: {
-                source: "title",
-                maxLength: 200
-            }
+          type: 'reference',
+          to: [
+            {
+              type: 'keyword',
+            },
+          ],
         },
-        {
-            name: "featured",
-            type: "boolean",
-            title: "Featured",
-        },
-        {
-            title: 'Banner',
-            name: 'banner',
-            type: 'image',
-        },
-        {
-            title: 'Content',
-            name: 'content',
-            type: 'array',
-            of: [{ type: 'block' }]
-        },
-        {
-            name: "category",
-            type: "reference",
-            title: "Category",
-            to: [
-                {
-                    type: "category",
-                }
-            ]
-        },
-        {
-            name: "keywords",
-            type: "array",
-            title: "Keywords",
-            of: [
-                {
-                    type: "reference",
-                    to: [
-                        {
-                            type: "keyword",
-                        },
-                    ],
-                },
-            ],
-        },
-    ]
-} 
+      ],
+    },
+  ],
+}
