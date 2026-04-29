6e12a1db9fdb76458188b0a1cc021528e8790798
Ketan Rajpal <ketanrajpal@gmail.com>
2026-04-29 21:12:27 +0100

    Refactor Card component styles for improved responsiveness and consistency

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,18 @@
 export const CardHeading = ({ title }: { title: string }) => (
-  <h3 className="font-serif text-3xl font-medium leading-relaxed tracking-wide text-pretty">
+  <h3 className="font-serif font-medium leading-relaxed tracking-wide text-pretty text-lg md:text-xl lg:text-2xl">
     {title}
   </h3>
 );
 
 export const CardParagraph = ({ description }: { description: string }) => (
-  <p className="text-lg font-medium leading-loose text-pretty tracking-wide text-zinc-500">
+  <p className="text-base md:text-md lg:text-lg font-medium leading-loose text-pretty tracking-wide text-zinc-500">
     {description}
   </p>
 );
 
 export const CardTag = ({ tag }: { tag: string }) => (
   <div>
-    <p className="inline-block rounded-lg bg-amber-300 p-1 px-3 font-bold uppercase tracking-wide text-black">
+    <p className="inline-block rounded-lg bg-amber-300 p-1 px-3 font-bold uppercase tracking-wide text-black text-sm md:text-sm lg:text-base">
       {tag}
     </p>
   </div>
