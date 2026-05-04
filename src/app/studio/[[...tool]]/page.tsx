a08dd34b1a1fa1dfb82d8de3e83fe67383eb8322
Ketan Rajpal <ketanrajpal@gmail.com>
2026-05-04 18:23:41 +0100

    Wrap NextStudio in fixed full-screen container
    Render NextStudio inside a fixed, full-viewport div (style: inset:0, position:fixed, zIndex:9999) so the studio overlays the app and occupies the entire screen. Also normalize import/export quoting and formatting (no functional changes) and keep dynamic export as "force-static".

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,18 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { NextStudio } from "next-sanity/studio";
 
-export const dynamic = 'force-static'
+import config from "../../../../sanity.config";
 
-export { metadata, viewport } from 'next-sanity/studio'
+export const dynamic = "force-static";
+
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return (
+    <div style={{ inset: 0, position: "fixed", zIndex: 9999 }}>
+      <NextStudio config={config} />
+    </div>
+  );
 }
